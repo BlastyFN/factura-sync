fbf21b4c8f9edbe56f027e5b8ec1dd6d71916401
BlastyFN <eblastjarna@gmail.com>
2023-06-12 10:48:40 -0600

    Interfaz de clientes v1.1

--- src/components/ClientsContainer.jsx
+++ src/components/ClientsContainer.jsx
@@ -2,22 +2,22 @@
 import React, {useState } from 'react';
 
 const ClientsContainer = () =>{
-    const [accepted, setAccepted] = useState(true);
+    const [active, setActive] = useState(false);
     const [filterText, setFilterText] = useState("");
    
-    const toggleAcceptance=()=> {
-        setAccepted(!accepted);
+    const toggleActivity=()=> {
+        setActive(!active);
     }
     return(
         <div className='ClientsContainer'> 
             <h1 className='text-center'>Clientes</h1>
-            <ClientsFilter isAccepted={accepted} onChangeSwitch={toggleAcceptance} onBtnSubmit={setFilterText}/>
-            <ClientBoard />
+            <ClientsFilter isActive={active} onChangeSwitch={toggleActivity} onBtnSubmit={setFilterText}/>
+            <ClientBoard isActive={active} />
         </div>
     );
 }
 
-const ClientsFilter = ({isAccepted, onChangeSwitch, onBtnSubmit}) =>{
+const ClientsFilter = ({isActive, onChangeSwitch, onBtnSubmit}) =>{
     //Estado local para poder limpiarlo cuando cambia el switch
     const [search, setSearch] = useState('');
     return(
@@ -29,7 +29,7 @@ const ClientsFilter = ({isAccepted, onChangeSwitch, onBtnSubmit}) =>{
             </div>
             <div className='row client-filter-row'>
                 <div className="switch col-2  d-inline-block">
-                    <input type="checkbox" id="switch-1" className="switch-input"  checked={isAccepted} onChange={()=>{
+                    <input type="checkbox" id="switch-1" className="switch-input"  checked={isActive} onChange={()=>{
                         onChangeSwitch();
                         setSearch("");
                         onBtnSubmit("");         
@@ -37,7 +37,7 @@ const ClientsFilter = ({isAccepted, onChangeSwitch, onBtnSubmit}) =>{
                     <label htmlFor="switch-1" className="switch-label"></label>
                     
                 </div>
-                <h3 className='d-inline-block col-10'>{isAccepted ? "Aceptados" : "No Aceptados"}</h3>
+                <h3 className='d-inline-block col-10'>{isActive ? "Activos" : "Inactivos"}</h3>
             </div>
             
             <div className='row client-filter-row'>
@@ -48,20 +48,21 @@ const ClientsFilter = ({isAccepted, onChangeSwitch, onBtnSubmit}) =>{
     );
 }
 
-const ClientBoard = () =>{
+const ClientBoard = ({isActive}) =>{
+    
     return(
         <div className='container overflow-hidden'>
             <div className='row row-cols-2 row-cols-lg-4 g-1 g-lg-2'>
-                <ClientCard />
-                <ClientCard />
-                <ClientCard />
-                <ClientCard />
-                <ClientCard />
-                <ClientCard />
-                <ClientCard />
-                <ClientCard />
-                <ClientCard />
-                <ClientCard />
+                <ClientCard isActive={isActive} />
+                <ClientCard isActive={isActive} />
+                <ClientCard isActive={isActive} />
+                <ClientCard isActive={isActive} />
+                <ClientCard isActive={isActive} />
+                <ClientCard isActive={isActive} />
+                <ClientCard isActive={isActive} />
+                <ClientCard isActive={isActive} />
+                <ClientCard isActive={isActive} />
+                <ClientCard isActive={isActive} />
 
 
             </div>
@@ -70,19 +71,19 @@ const ClientBoard = () =>{
     );
 }
 
-const ClientCard = ()=>{
+const ClientCard = ({isActive, client})=>{
     return(
         <div className='col mb-1'>
             <div className='ClientCard col'>
                 <div className='ClientCard-header p-2'>
-                    <h3>BlastyFN</h3>
+                    <h5>Alex Programador</h5>
                 </div>
                 <div className='ClientCard-body p-2'>
                     <p >Alejandro Francisco Bañuelos Romero</p>
                     <p>eduardofina7@gmail.com</p>
                 </div>
                 <div className='ClientCard-footer p-2 d-grid gap-2 col-6 mx-auto'>
-                    <button className='btn btn-lg btn-green'>Aceptar</button>
+                    <button className={"btn btn-lg " + (isActive?"btn-red":"btn-green")}>{isActive?"Desactivar":"Activar"}</button>
                 </div>
                 
             </div>
